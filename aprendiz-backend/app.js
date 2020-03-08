107b98e50ff9c7cf53df9b85d37327803114d2aa
Renan <sherlockimports1@gmail.com>
2020-03-08 11:01:27 -0300

    Tentando por o gitignore

--- aprendiz-backend/app.js
+++ aprendiz-backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 //Rotas
 const router = express.Router();
 router.get("/", (req, res) => {
-    res.send("Olá Mundo!");
+    res.send("Olá Mundo! 2.0");
 });
 
 //Configurações
